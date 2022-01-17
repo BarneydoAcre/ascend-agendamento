7c55c39168ba9e93140155b0a32e9e535a47b72e
unknown <gabrielfauth@outlook.com>
2022-01-16 20:36:06 -0400

    debug bugou

--- agendamentos/settings.py
+++ agendamentos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0_3o!2b@i$lb=(*%#clzq(n^-uh%)#vkwcl*i(puk&2$9qt#u='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
